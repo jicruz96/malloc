aa3a6169789026d6d7557969963cb5b797ae4938
jicruz96 <accounts@jicruz.com>
2021-06-08 18:26:35 +0000

    Update malloc.c

--- malloc.c
+++ malloc.c
@@ -6,6 +6,10 @@
 
 #define shift_address(addr, size) \
 	((void *)((char *)(addr) + (size)))
+#define is_allocated(x) ((x) & 1)
+#define sizeof_chunk(x) ((x) ^ 1)
+#define allocate(x) (*((char *)(x)) += 1)
+#define unallocate(x) (*((char *)(x)) -= 1)
 
 /**
  * align - returns the page-aligned value of a quantity
@@ -27,7 +31,7 @@ size_t align(size_t size)
 }
 
 /**
- * naive_malloc - naive version of malloc
+ * _malloc - cusotm-built malloc
  * @size: number of bytes to allocate
  *
  * Return: the memory address newly allocated, or NUll on error
@@ -38,27 +42,38 @@ void *_malloc(size_t size)
 {
 	size_t chunk_size = align(size + sizeof(size_t));
 	static void *next_spot;
-	static size_t allocated;
+	static size_t available, allocated;
 	void *tmp;
 	static long pagesize;
 
 	if (size == 0)
 		return (NULL);
 
-	for (; allocated < chunk_size; allocated += pagesize)
+	while (available < chunk_size)
+	{
 		if (!next_spot)
 		{
 			pagesize = sysconf(_SC_PAGESIZE);
 			next_spot = sbrk(pagesize);
+			if (next_spot == (void *)-1)
+			{
+				next_spot = NULL;
+				return (NULL);
+			}
 		}
-		else
+		else if (sbrk(pagesize) == (void *)-1)
 		{
-			sbrk(pagesize);
+			return (NULL);
 		}
+
+		allocated += pagesize;
+		available += pagesize;
+	}
 
 	tmp = next_spot;
 	memcpy(tmp, &chunk_size, sizeof(chunk_size));
+	allocate(tmp);
 	next_spot = shift_address(tmp, chunk_size);
-
+	available -= chunk_size;
 	return (shift_address(tmp, sizeof(chunk_size)));
 }
