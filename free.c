6c9c68d1bb667ea9c3a6e16d1a918c79e4a1af0b
jicruz96 <accounts@jicruz.com>
2021-06-09 07:01:51 +0000

    First commit of free.c for Task 2

---
+++ free.c
@@ -0,0 +1,12 @@
+#include "malloc.h"
+
+/**
+ * _free - free replica
+ * @ptr: pointer to free
+ **/
+void _free(void *ptr)
+{
+	unallocate(ptr);
+	if (sizeof_chunk(ptr) > largest)
+		largest = sizeof_chunk(ptr);
+}
